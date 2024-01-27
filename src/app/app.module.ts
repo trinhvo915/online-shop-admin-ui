77f3c086930db86399c5b35d53f419c78bef67b1
vovantrinhdhbkdn@gmail.com <Trinh Vo>
2024-01-27 16:50:02 +0700

    configure ant design in source angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,15 @@ import { ErrorComponent } from './pages/error/error.component';
 import { LayoutModule } from './layout/layout.module';
 import { AnalyticsComponent } from './pages/analytics/analytics.component';
 import { CategoryComponent } from './pages/category/category.component';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { en_US } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NzTabsModule } from 'ng-zorro-antd/tabs';
+registerLocaleData(en);
 
 @NgModule({
   declarations: [
@@ -19,9 +28,17 @@ import { CategoryComponent } from './pages/category/category.component';
     AnalyticsComponent,
     CategoryComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, LayoutModule],
+  imports: [
+    BrowserModule, 
+    AppRoutingModule, 
+    LayoutModule, 
+    FormsModule, 
+    HttpClientModule, 
+    BrowserAnimationsModule,
+    NzTabsModule
+  ],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
-  providers: [LayoutModule],
+  providers: [LayoutModule, { provide: NZ_I18N, useValue: en_US }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
